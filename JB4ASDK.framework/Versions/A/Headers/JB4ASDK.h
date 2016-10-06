13d3b3abdd51e899c476455b26b7bb3ef40caf8f
Maksym Grebenets <maksym.grebenets@cba.com.au>
2016-10-06 11:46:36 +1100

    Add framework

---
+++ JB4ASDK.framework/Versions/A/Headers/JB4ASDK.h
@@ -0,0 +1,26 @@
+//
+//  JB4ASDK.h
+//
+//  JB4A iOS SDK GitHub Repository
+//  https://salesforce-marketingcloud.github.io/JB4A-SDK-iOS/
+
+//  Copyright © 2016 Salesforce Marketing Cloud. All rights reserved.
+//
+#import <Foundation/Foundation.h>
+
+#import <JB4ASDK/ETPush.h>
+#import <JB4ASDK/ETAnalytics.h>
+#import <JB4ASDK/ETLocationManager.h>
+#import <JB4ASDK/ETEventRegion.h>
+#import <JB4ASDK/ETGenericUpdate.h>
+#import <JB4ASDK/ETLandingPagePresenter.h>
+#import <JB4ASDK/ETMessage.h>
+#import <JB4ASDK/ETRegion.h>
+#import <JB4ASDK/ExactTargetEnhancedPushDataSource.h>
+#import <JB4ASDK/ETCloudPage.h>
+#import <JB4ASDK/ETCloudPageDataSource.h>
+#import <JB4ASDK/ETEventMessage.h>
+#import <JB4ASDK/ETWKLandingPagePresenter.h>
+#import <JB4ASDK/PICart.h>
+#import <JB4ASDK/PICartItem.h>
+#import <JB4ASDK/PIOrder.h>
